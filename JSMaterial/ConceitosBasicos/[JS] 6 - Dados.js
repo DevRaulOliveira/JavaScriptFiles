8fddc4697235d3a789ff3984f98db981a43aa37f
DevRaulOliveira <raul_cez@outlook.com>
2024-12-08 07:24:41 -0300

    Upload

---
+++ JSMaterial/ConceitosBasicos/[JS] 6 - Dados.js
@@ -0,0 +1,14 @@
+// Tipos de dados
+// Conceitos básicos de dados e variáveis que serão mostrados abaixo
+
+// 1 - Criando variáveis.
+a = 23; // Variável a, recebe o valor 23
+b = 22; // Variável b, recebe o valor 22
+
+// 2 - Realizando a impressão de alguns tipos de variáveis.
+console.log('Palavra'); // É uma string
+console.log('Neste caso o numero','2','também é uma string');
+console.log('Neste casoo é um valor int',10); // Neste caso 10 tem um valor numérico inteiro
+console.log('Neste caso é um float',10.0); // Neste caso é um float
+console.log('Neste caso foi realizado uma soma de 2 +10, sendo igual a:',2+10);
+console.log('A soma de a + b é igual a',a+b);
