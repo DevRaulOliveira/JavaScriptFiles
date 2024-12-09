68490f579b83112102a802e51a31c05658b69655
Raul Oliveira <168892121+DevRaulOliveira@users.noreply.github.com>
2024-12-09 08:03:03 -0300

    Create [JS] 3 - DesafioTrocaValor.js

---
+++ JSMaterial/Desafios/[JS] 3 - DesafioTrocaValor.js
@@ -0,0 +1,20 @@
+/*
+ Desafio 3 - Troca de valores
+
+ Basicamente é preciso trocar os valores entre as variáveis, fazendo com que A tenha o valor de B e B o valor de A
+
+ a = 7
+ b = 94
+
+*/
+
+let a = 7;
+let  b = 94;
+let temp = a; // Salva o antigo valor de a dentro da variável nova assim o valor antigo de a permanece guardado.
+
+console.log('A vale', a,'e B vale', b);
+
+a =b; 
+b = temp; // Resgata o antig valor de a que está armazenado na variável temporária
+
+console.log('O novo valor de A é', a,'e o novo valor de B é',b);
