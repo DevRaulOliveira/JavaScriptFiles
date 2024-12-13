d718119cc13101ab47939f0b823ba2beac79747e
DevRaulOliveira <raul_cez@outlook.com>
2024-12-12 22:08:18 -0300

    Upload

---
+++ JSMaterial/Fundamentos/[JS] 1 - Organizacao.js
@@ -0,0 +1,11 @@
+// Organização de código em Java Script
+/*
+É interessante realizar a separação por utilização de blocos, usando chaves {}
+*/
+
+{ // Um bloco de código
+    { // Outro bloco de código
+        console.log('Olá');
+        console.log('mundo!');
+    }
+}
