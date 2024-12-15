2946f6517b284f9ff5a61ca556dcb95eb148e803
DevRaulOliveira <raul_cez@outlook.com>
2024-12-15 16:33:56 -0300

    Upload

---
+++ JSMaterial/Fundamentos/[JS] 14 - Booleanos.js
@@ -0,0 +1,21 @@
+// Tipo booleano
+// Retorna TRUE ou FALSE. Retorna também 1 e 0
+
+// Variáveis
+
+let isAtivo = false;
+console.log('Imprimindo a variavel isAtivo temos:\n',isAtivo);
+
+// Mudando o valor da variável para true
+isAtivo = true;
+console.log('\nApós a mudança do valor da variável:\n',isAtivo)
+
+
+// Metodo boolean
+isAtivo = 1
+console.log('\nUtilizando o metodo Boolean temos que 1 é:\n',Boolean(isAtivo));
+
+// Utilizando negações usando exclamação [!]
+console.log('\nA variável não é verdadeira:\n',!isAtivo,'\nNão, a variável é verdadeira, por ter valor 1.') // Retorna um teste lógico de verdadeiro ou falso
+
+// Por via de regra todos os numeros inteiros são verdadeiros menos o 0
