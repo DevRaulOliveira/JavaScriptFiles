cee51bda0f80c49377d131d861967920b238ce4a
DevRaulOliveira <raul_cez@outlook.com>
2024-12-15 17:43:44 -0300

    Upload

--- JSMaterial/Fundamentos/[JS] 14 - Booleanos.js
+++ JSMaterial/Fundamentos/[JS] 14 - Booleanos.js
@@ -19,3 +19,10 @@ console.log('\nUtilizando o metodo Boolean temos que 1 é:\n',Boolean(isAtivo));
 console.log('\nA variável não é verdadeira:\n',!isAtivo,'\nNão, a variável é verdadeira, por ter valor 1.') // Retorna um teste lógico de verdadeiro ou falso
 
 // Por via de regra todos os numeros inteiros são verdadeiros menos o 0
+
+// Realizando uma operação lógica encima de um String
+let nome = 'Pedro';
+console.log('Realizando teste lógico usando variáve: ',nome || 'Desconhecido');
+
+nome = '' // Retirando o valor de dentro da variável nome
+console.log(`\nTeste logico retorando falso, ou seja segunda opção: ${nome || 'Desconhecido'}`) // Teste lógico falso, retornando a segunda opção
